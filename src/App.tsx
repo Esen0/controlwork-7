f2ab2c3013204cafb39b091f410ef6f96f68c8e8
uzakbekov esentur <esentur_03@mail.ru>
2024-06-01 16:29:03 +0600

    добавил основную логику и удаления элементов

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
-import './App.css'
+import React, {useState} from 'react';
+import './App.css';
+import OrderDatails from './components/OrderDatails/OrderDatails';
+import AddItems from './components/AddItems/AddItems';
 
-const App = () => {
+interface MenuItem {
+  name: string;
+  price: number;
+}
+
+const menuItems: MenuItem[] = [
+  {name: 'Hamburger', price: 80},
+  {name: 'CheeseBurger', price: 90},
+  {name: 'Fries', price: 50},
+  {name: 'Cola', price: 40},
+  {name: 'Nuggest', price: 70},
+  {name: 'Salad', price: 60},
+];
+
+interface OrderItem extends MenuItem {
+  quantity: number;
+}
+
+const App: React.FC = () => {
+  const [order, setOrder] = useState<OrderItem[]>([]);
+
+  const handleAddItem = (name: string) => {
+    setOrder((prevOrder) => {
+      const existingItem = prevOrder.find((item) => item.name === name);
+      if(existingItem) {
+        return prevOrder.map((item) =>
+          item.name === name ? {...item, quantity: item.quantity + 1} : item,
+        );
+      } else {
+        const menuItem = menuItems.find((item) => item.name === name)!;
+        return [...prevOrder, {...menuItem, quantity: 1}];
+      }
+    });
+  };
+
+  const handleRemoveItem = (name: string) => {
+    setOrder((prevOrder) => prevOrder.filter(item => item.name !== name));
+  };
 
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className='app'>
+      <OrderDatails items={order} onRemoveItem={handleRemoveItem} />
+      <AddItems items={menuItems} onAddItem={handleAddItem} />
+    </div>
+  );
+};
 
 export default App
